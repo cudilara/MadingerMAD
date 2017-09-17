0dbccf57e494ca20f5d72843fb908ab114572636
cudilara <dilara.madinger@colorado.edu>
2017-09-16 18:36:17 -0600

    Images
    Added images.

---
+++ lab2/madinger_lab2/madinger_lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  madinger_lab2
+//
+//  Created by Dilara Madinger on 9/16/17.
+//  Copyright © 2017 Dilara Madinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
