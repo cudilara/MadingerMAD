4718a7862d63fbb50a90a19e0f9a97b5708ed905
cudilara <dilara.madinger@colorado.edu>
2017-09-20 23:45:09 -0600

    Lab 2

--- lab2/madinger_lab2/madinger_lab2/ViewController.swift
+++ lab2/madinger_lab2/madinger_lab2/ViewController.swift
@@ -10,6 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var imageControl: UISegmentedControl!
+    @IBOutlet weak var capitalSwitch: UISwitch!
+    @IBOutlet weak var colorSwitch: UISwitch!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    func updateImage(){
+        if imageControl.selectedSegmentIndex==0{
+            mainLabel.text="Leia"
+            mainImage.image=UIImage(named: "leia")
+        }
+        else if imageControl.selectedSegmentIndex==1 {
+            mainLabel.text="Rey"
+            mainImage.image=UIImage(named: "ray")
+        }
+    }
+    func updateCaps(){
+        if capitalSwitch.isOn {
+            mainLabel.text=mainLabel.text?.uppercased()
+        } else {
+            mainLabel.text=mainLabel.text?.lowercased()
+        }
+
+    }
+    func changeColor(){
+        if colorSwitch.isOn{
+            mainLabel.textColor=UIColor.red
+        } else {
+            mainLabel.textColor=UIColor.black
+        }
+        
+    }
+    @IBAction func changeFontSize(_ sender: UISlider) {
+        let fontSize=sender.value
+        fontSizeLabel.text=String(format: "%.0f", fontSize)
+        mainLabel.font=UIFont.systemFont(ofSize: CGFloat(fontSize))
+    }
+    @IBAction func updateFont(_ sender: UISwitch) {
+            updateCaps()
+    }
+    @IBAction func updateColor(_ sender: UISwitch) {
+        changeColor()
+    }
+    @IBAction func changeImage(_ sender: UISegmentedControl) {
+        updateImage()
+        updateCaps()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
